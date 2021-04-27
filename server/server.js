93d37ade7e9a3a8e8b10aa80f5732f74796143b0
Berkay Baltas <berkay@Berkays-MacBook-Pro.local>
2021-04-27 13:31:59 -0400

    add MongoDB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 import express from "express";
-
+import bodyParser from "body-parser";
+import cors from "cors";
 const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("yo");
+});
+
+app.post("/api/register", (req, res) => {
+  res.json({ status: "ok" });
 });
 
 app.listen(3000);
